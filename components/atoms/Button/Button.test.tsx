fae8228db126157bb0ebd6c9da50c8270b70bf14
Taeho Jo <taeho.jo@dbdlab.io>
2022-02-16 15:47:36 +0900

    feat: husky test commit

--- components/atoms/Button/Button.test.tsx
+++ components/atoms/Button/Button.test.tsx
@@ -2,7 +2,7 @@ import Button from './index';
 import { render } from '@testing-library/react';
 import React from 'react';
 
-describe('BUTTON!!!!!!!!!!!!!!!!!', () => {
+describe('atoms/Button', () => {
   it('matches snapshot', () => {
     const utils = render(
       <Button size={'sm'} bgColor={'primary'} loading={true} active={false} />,
