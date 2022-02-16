779cddcecf7f2c529859deb5337a82ec289a045a
Taeho Jo <taeho.jo@dbdlab.io>
2022-02-16 17:09:57 +0900

    feat: test

--- components/atoms/Button/Button.test.tsx
+++ components/atoms/Button/Button.test.tsx
@@ -2,11 +2,33 @@ import Button from './index';
 import { render } from '@testing-library/react';
 import React from 'react';
 
-describe('atoms/Button', () => {
-  it('matches snapshot', () => {
+describe('Button', () => {
+  it('loading button', () => {
     const utils = render(
-      <Button size={'sm'} bgColor={'primary'} loading={true} active={false} />,
+      <Button loading={true} />,
+    );
+    expect(utils.container).toMatchSnapshot();
+  });
+
+  it('active button', () => {
+    const utils = render(
+      <Button active={true} />,
+    );
+    expect(utils.container).toMatchSnapshot();
+  });
+
+  it('large button', () => {
+    const utils = render(
+      <Button size={'lg'} />,
+    );
+    expect(utils.container).toMatchSnapshot();
+  });
+
+  it('border button', () => {
+    const utils = render(
+      <Button border={'none'} />,
     );
     expect(utils.container).toMatchSnapshot();
   });
 });
+
