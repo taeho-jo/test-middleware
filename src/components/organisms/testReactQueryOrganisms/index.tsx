e9d13a3921b2dc92f6e51fa2a0af960da0b0779a
Taeho Jo <taeho.jo@dbdlab.io>
2022-02-23 21:30:06 +0900

    feat:test

--- src/components/organisms/testReactQueryOrganisms/index.tsx
+++ src/components/organisms/testReactQueryOrganisms/index.tsx
@@ -3,64 +3,69 @@ import PageTitle from '../../atoms/PageTitle';
 import Icon from '../../atoms/Icon';
 import ClipLoader from 'react-spinners/ClipLoader';
 import FlexBox from '../../atoms/FlexBox';
-import { useGetHooks } from '../../../hooks/useGetHooks';
+import { useGetHooks, useMultipleGetHooks } from '../../../hooks/useGetHooks';
 import { usePostHooks } from '../../../hooks/usePostHooks';
 import Button from '../../atoms/Button';
 
 const TestReactQueryOrganisms = () => {
-  const { isLoading, isError, error, data } = useGetHooks(
-    ['products'],
-    '/products',
-    {},
-  );
+  const data = useGetHooks(['products'], '/products', {
+    onError: e => {
+      console.error('ProductAPI::::::::: ', e);
+    },
+  });
+  // const data2 = useGetHooks(['category'], '/products/categories', {});
+  //
+  // const dataAll = useMultipleGetHooks(
+  //   ['products', 'category'],
+  //   ['/products', '/products/categories'],
+  //   [{}, {}],
+  // );
+  //
+  // console.log('개별로 각각 호출 data ::: ', data);
+  // console.log('개별로 각각 호출 data2 ::: ', data2);
+  // console.log('전체로 한번에 호출 dataAll::::: ', dataAll);
 
-  const {
-    isLoading: isLoadingCategory,
-    isError: isErrorCategory,
-    error: errorCategory,
-    data: dataCategory,
-  } = useGetHooks(['category'], '/products/categories', { enabled: false });
+  // console.log(data2, '16847');
+  // const {
+  //   data: dataLogin,
+  //   isError: isErrorLogin,
+  //   error: errorLogin,
+  //   refetch,
+  // } = usePostHooks(
+  //   ['login'],
+  //   '/auth/login',
+  //   { username: 'mor_2314', password: '83r5^_' },
+  //   { enabled: false },
+  // );
+  //
+  // useEffect(() => {
+  //   if (dataLogin) {
+  //     alert(dataLogin.data.token);
+  //   }
+  // }, [dataLogin]);
 
-  const {
-    data: dataLogin,
-    isError: isErrorLogin,
-    error: errorLogin,
-    refetch,
-  } = usePostHooks(
-    ['login'],
-    '/auth/login',
-    { username: 'mor_2314', password: '83r5^_' },
-    { enabled: false },
-  );
-
-  useEffect(() => {
-    if (dataLogin) {
-      alert(dataLogin.data.token);
-    }
-  }, [dataLogin]);
-
-  if (isLoading) {
+  if (data.isLoading) {
     return (
       <>
         <FlexBox>
-          <ClipLoader color={'red'} loading={isLoading} size={150} />
+          <ClipLoader color={'red'} loading={data.isLoading} size={150} />
         </FlexBox>
       </>
     );
   }
-  if (isError) {
-    return <span>Error: {error['message']}</span>;
+  if (data.isError) {
+    return <span>Error: {data.error['message']}</span>;
   }
 
   return (
     <>
       <PageTitle title={'React Qeury Test Page'} />
-      <Button size={'md'} type={'button'} onClick={refetch}>
-        버튼!!!!!!!!!
-      </Button>
+      {/*<Button size={'md'} type={'button'} onClick={refetch}>*/}
+      {/*  버튼!!!!!!!!!*/}
+      {/*</Button>*/}
       <Icon name="TEST" size={30} />
       <FlexBox direction={'column'}>
-        {data?.data.map(product => {
+        {data?.data?.data?.map(product => {
           return (
             <Fragment key={product.id}>
               <div>{product.title}</div>
--- src/hooks/useGetHooks.ts
+++ src/hooks/useGetHooks.ts
@@ -8,3 +8,21 @@ export const useGetHooks = (queryKey, path, config) => {
 
   return useQuery(queryKey, getFetch, config);
 };
+
+export const useMultipleGetHooks = (queryKey, path, config) => {
+  const getMultipleFetch = async url => {
+    return await AXIOS.get(url);
+  };
+
+  const resultArr = {};
+  for (let i = 0; i < queryKey.length; i++) {
+    const res = useQuery(
+      [queryKey[i]],
+      () => getMultipleFetch(path[i]),
+      config[i],
+    );
+    resultArr[queryKey[i]] = res;
+  }
+
+  return resultArr;
+};
