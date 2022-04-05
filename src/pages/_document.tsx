b533a19dbb5b5d30140770027602a39f8b6cfe16
Taeho Jo <taeho.jo@dbdlab.io>
2022-04-05 18:16:43 +0900

    feat: seo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,16 +18,19 @@ class MyDocument extends Document {
           <meta httpEquiv="expires" content="0" />
           <meta httpEquiv="pragma" content="no-cache" />
 
-          <title>Diby</title>
-          <meta name="description" content="CX 리서치 솔루션, Diby" />
+          {/*<title>Diby | 일잘하는 당신을 위한 CX 리서치 솔루션</title>*/}
+          {/*<meta*/}
+          {/*  name="description"*/}
+          {/*  content="유저테스트로 서비스를 사용한 잠재고객의 피드백을 수집합니다. Diby 에서 UX 리서치 설계부터 응답자 보상 지급, 문장형 데이터를 분석하고 업무 효율성을 높이세요."*/}
+          {/*/>*/}
           <link rel="icon" href="/favicon.png" />
 
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="Diby" />
-          <meta property="og:url" content="https://diby.io" />
-          <meta property="og:image" content="/favicon.png" />
-          <meta property="og:description" content="CX 리서치 솔루션, Diby" />
-          <meta property="og:site_name" content="Diby" />
+          {/*<meta property="og:title" content="Diby" />*/}
+          {/*<meta property="og:url" content="https://diby.io" />*/}
+          <meta property="og:image" content="/og-diby.png" />
+          {/*<meta property="og:description" content="CX 리서치 솔루션, Diby" />*/}
+          {/*<meta property="og:site_name" content="Diby" />*/}
           <meta property="og:locale" content="ko_KR" />
           {/*다음의 태그는 필수는 아니지만, 포함하는 것을 추천함*/}
           {/*<meta property="og:image:width" content="1200">*/}
