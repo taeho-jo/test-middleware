7686cdabe80d6f42a372f3200cf88764d19fcb71
Taeho Jo <taeho.jo@dbdlab.io>
2022-04-05 19:13:02 +0900

    feat: seo 1차 적용

--- src/pages/pricing/index.tsx
+++ src/pages/pricing/index.tsx
@@ -10,7 +10,7 @@ const Pricing = () => {
         {/*<meta name="description" content="시장조사, 핵심 고객 정의" />*/}
 
         <meta property="og:title" content="가격 안내, Diby" />
-        {/*<meta property="og:description" content="시장조사, 핵심 고객 정의" />*/}
+        <meta property="og:description" content="가격 안내" />
         <meta property="og:url" content="https://diby.io/pricing" />
         <meta property="og:site_name" content="가격 안내, Diby" />
       </Head>
