aa71ee33d76144843a5820d7366f39f36a12f21d
Taeho Jo <taeho.jo@dbdlab.io>
2022-09-30 15:30:20 +0900

    fix: 권한 없음 수정

--- src/store/sagas/commonSaga/commonSaga.ts
+++ src/store/sagas/commonSaga/commonSaga.ts
@@ -2,8 +2,6 @@ import { call, delay, put, takeEvery } from '@redux-saga/core/effects';
 import { getCommonCode, getErrorInfo, updateCommonCode } from '../../reducers/commonReducer';
 import { fetchCommonCodeApi } from '../../../api/authApi';
 import { getRefreshToken } from '../../reducers/authReducer';
-import { getUserInfo } from '../../reducers/userReducer';
-import { clearLocalStorage } from '../../../common/util/commonFunc';
 
 function* getCommonCodeSaga() {
   try {
@@ -19,10 +17,6 @@ function* getCommonCodeSaga() {
       yield put(getRefreshToken());
       yield delay(1000);
       yield put(getCommonCode());
-    }
-    if (e?.response?.data?.code === 'E0002') {
-      clearLocalStorage();
-      window.location.href = 'https://stag.diby.io';
     }
     yield put(getErrorInfo(e));
   }
